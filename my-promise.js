9879e91437af3c1a7c6bc3553db1f31ba947704d
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-09-16 13:56:25 +0800

    添加resolve和reject函数框架

--- my-promise.js
+++ my-promise.js
@@ -5,4 +5,17 @@ function Promise(executor) {
     self.onResolvedCallback = [];   //Promise resolve时的回调集,因为Promise结束之前可能有多个回调添加在它上面
     self.onRejectedCallback = [];   //Promise reject时的回调集,因为Promise结束之前可能有多个回调添加在它上面
     executor(resolve, reject);
+    
+    function resolve(value) {
+        //TODO
+    }
+    function reject(reason) {
+        //TODO
+    }
+    try{    //考虑到executor执行过程中可能会出错,所以使用try/catch包起来,如果出错,reject这个promise
+        executor(resolve, reject);
+
+    } catch (e){
+        reject(e);
+    }
 }
