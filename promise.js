0291ea51db5d4d193b637d43de704c6663c88b72
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-01-17 10:12:42 +0800

    接力

--- promise.js
+++ promise.js
@@ -4,13 +4,23 @@ function Promise(fn) {
         deferreds = [];
 
     this.then = function (onFulfilled) {
-        if (state === 'pending') {
-            deferreds.push(onFulfilled);
-            return this;
-        }
-        onFulfilled(value);
-        return this;
+        return new Promise(function (resolve) {
+            handle({
+                onFulfilled: onFulfilled || null,
+                resolve: resolve
+            });
+        });
     };
+
+    function handle(deferred) {
+        if (state === 'pending') {
+            deferreds.push(deferred);
+            return;
+        }
+
+        var ret = deferred.onFulfilled(value);
+        deferred.resolve(ret);
+    }
 
     function resolve(newValue) {
         value = newValue;
